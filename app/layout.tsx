07e5ac8536c1f0b7f415c443597ff505999ddaf5
RyfterWasTaken <antoine.f.salaun@gmail.com>
2024-03-23 11:38:16 +0100

    modified:   app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,11 +11,6 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const webhook = "https://canary.discord.com/api/webhooks/1221035576010997822/8doMx9QwPJ9mpkRwKY5961v_FMLbKkGAKJiIasaNjT0wH4BbkEkzdhCjzhXt9Nlhmo34";
-  const body = {
-    "content": "logged entry"
-  }
-  await fetch(webhook,{body: JSON.stringify(body), headers:{"content-type":"application/json"}, method:"POST"});
   return (
     <html lang="en">
       {children}
